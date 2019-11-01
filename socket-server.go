951caba74572998eddb1a88a01461b90f7c9eebe
reza eskandari <rezaesskandari@gmail.com>
2019-11-02 00:57:10 +0330

    add user model

--- socket-server.go
+++ socket-server.go
@@ -1,13 +1,46 @@
 package main
 
 import (
-	"./config"
-	"flag"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	app := &config.App{}
-	port := flag.Int("port", 8080, "run application on given port")
-	flag.Parse()
-	app.Run(*port)
+	//app := &config.App{}
+	//port := flag.Int("port", 8080, "run application on given port")
+	//flag.Parse()
+	//app.Run(*port)
+
+	i := 0
+	g := make(chan interface{})
+
+	go func() {
+		i++
+		data := fmt.Sprintf("%s-%d", "Greet", i)
+		greet(g, data)
+	}()
+
+	go func() {
+		for {
+			select {
+			case a := <-g:
+				fmt.Println(a)
+			}
+		}
+	}()
+
+	var a string
+	fmt.Scanln(&a)
+
+}
+
+func greet(c chan interface{}, data interface{}) {
+	t := time.Duration(time.Second * 3)
+	tick := time.Tick(t)
+	for range tick {
+		c <- rand.Float32()
+	}
+
+	close(c)
 }

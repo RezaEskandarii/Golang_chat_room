928303efe8a8ab368c2e2e771163cae632a7d25e
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2019-11-24 22:29:34 +0330

    Update socket-server.go

--- socket-server.go
+++ socket-server.go
@@ -11,6 +11,7 @@ func main() {
 	app := &config.App{}
 	
 	// get port number from user's cli
+	// default port number is 8080
 	port := flag.Int("port", 8080, "run application on given port")
 	flag.Parse()
 	app.Run(*port)

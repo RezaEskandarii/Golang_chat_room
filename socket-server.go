ef419defd4836b6f180418affece6d88aad42330
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2019-11-24 22:28:57 +0330

    Update socket-server.go

--- socket-server.go
+++ socket-server.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	// container
 	app := &config.App{}
+	
+	// get port number from user's cli
 	port := flag.Int("port", 8080, "run application on given port")
 	flag.Parse()
 	app.Run(*port)

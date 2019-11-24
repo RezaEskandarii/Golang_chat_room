c33f37f94b5dd80fae23fe9a4f04380ca7a26d97
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2019-11-24 22:26:47 +0330

    Update socket-server.go

--- socket-server.go
+++ socket-server.go
@@ -7,40 +7,10 @@ import (
 )
 
 func main() {
-	//app := &config.App{}
-	//port := flag.Int("port", 8080, "run application on given port")
-	//flag.Parse()
-	//app.Run(*port)
-
-	i := 0
-	g := make(chan interface{})
-
-	go func() {
-		i++
-		data := fmt.Sprintf("%s-%d", "Greet", i)
-		greet(g, data)
-	}()
-
-	go func() {
-		for {
-			select {
-			case a := <-g:
-				fmt.Println(a)
-			}
-		}
-	}()
-
-	var a string
-	fmt.Scanln(&a)
-
+	app := &config.App{}
+	port := flag.Int("port", 8080, "run application on given port")
+	flag.Parse()
+	app.Run(*port)
 }
 
-func greet(c chan interface{}, data interface{}) {
-	t := time.Duration(time.Second * 3)
-	tick := time.Tick(t)
-	for range tick {
-		c <- rand.Float32()
-	}
-
-	close(c)
-}
+ 
